a5a52e30ebb279c3dc242ad696e60d610851d136
Yousuf Qutubuddin <Uqutub@qutbitech.local>
2018-08-30 16:47:38 +0500

    Add All Scripts for client

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -20,6 +21,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
 // error handler
 app.use(function(err, req, res, next) {
--- server_script.sh
+++ server_script.sh
@@ -2,3 +2,4 @@ git checkout $1 && git pull origin $1
 sh ./dockerBuild.sh
 # sh ./loginDocker.sh
 sh ./dockerPushToRepo.sh
+sh ./updateKubectl.sh
